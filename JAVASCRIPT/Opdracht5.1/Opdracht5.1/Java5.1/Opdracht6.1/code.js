a69ba8b111c63019536c33e2d47cebf25c689eeb
9021948 <144111943+9021948@users.noreply.github.com>
2023-10-04 09:05:07 +0200

    som gevoegd

--- JAVASCRIPT/Opdracht5.1/Opdracht5.1/Java5.1/Opdracht6.1/code.js
+++ JAVASCRIPT/Opdracht5.1/Opdracht5.1/Java5.1/Opdracht6.1/code.js
@@ -8,3 +8,11 @@ function verandertekst()
 {
     document.getElementById("id1").innerHTML = "Tekst is Veranderd!!!";
 }
+
+
+function Calculate(getal1,getal2)
+{
+    antwoord = getal1 + getal2;
+    console.log(antwoord)
+}
+
