dc563048e37a072a1230ea3792a2f5673bb0bc07
9021948 <144111943+9021948@users.noreply.github.com>
2023-10-05 11:29:49 +0200

    box in center met kleur dus af

--- JAVASCRIPT/Opdracht5.1/Opdracht5.1/Java5.1/Opdracht6.1/code.js
+++ JAVASCRIPT/Opdracht5.1/Opdracht5.1/Java5.1/Opdracht6.1/code.js
@@ -12,7 +12,12 @@ function verandertekst()
 
 function Calculate(getal1,getal2)
 {
+    getal1 = parseInt (prompt("geef getal 1" , ""),10);
+    getal2 = parseInt (prompt("geef getal 1" , ""),10);
+    
+
     antwoord = getal1 + getal2;
     console.log(antwoord)
+    document.write("de som is: " + getal1 + "+" + getal2 + "= " + antwoord");
 }
 
